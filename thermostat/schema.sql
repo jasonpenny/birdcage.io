ab95c4570cc0e0bc57456b87ffb90ec7f908a046
Jason Penny <jasonpenny4@gmail.com>
2016-10-13 17:13:08 -0400

    Thermostat allow setting target temperature, getting current target temperature and getting change history of target temperature

--- thermostat/schema.sql
+++ thermostat/schema.sql
@@ -3,3 +3,9 @@ CREATE TABLE info(
     unique_id text,
     nickname text
 );
+
+DROP TABLE IF EXISTS target_temperatures;
+CREATE TABLE target_temperatures (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    temperature integer
+);
--- thermostat/views.py
+++ thermostat/views.py
@@ -31,3 +31,45 @@ def update_nickname():
 def current_temperature():
     temp = random.randint(65, 75)
     return jsonify(temperature=temp)
+
+@app.route('/target_temperatures/current', methods=['GET'])
+def get_current_target_temperature():
+    db = get_db()
+    cur = db.execute(
+        'SELECT temperature '
+        'FROM   target_temperatures '
+        'ORDER BY id DESC '
+        'LIMIT 1')
+    rec = cur.fetchone()
+    if not rec:
+        return jsonify(temperature=None)
+
+    return jsonify(temperature=rec['temperature'])
+
+@app.route('/target_temperatures/current', methods=['POST'])
+def set_current_target_temperature():
+    data = request.get_json()
+
+    if not data or \
+       not data.get('temperature'):
+        return (
+            jsonify(success=False,
+                    error="'temperature' field is required"),
+            400)
+
+    db = get_db()
+    db.execute('INSERT INTO target_temperatures (temperature)'
+               'VALUES (?)',
+               [data['temperature']])
+    db.commit()
+
+    return jsonify(success=True)
+
+@app.route('/target_temperatures')
+def get_target_temperature_history():
+    db = get_db()
+    cur = db.execute('SELECT temperature '
+                     'FROM   target_temperatures '
+                     'ORDER BY id ')
+    entries = [dict(row) for row in cur.fetchall()]
+    return jsonify(entries)
